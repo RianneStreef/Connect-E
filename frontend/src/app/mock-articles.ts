cfaeeea66f13657423a07c2f7f8f4dd67f0cb6aa
RianneStreef <riannestreef@gmail.com>
2020-12-24 14:59:02 +0100

    meeting 2412

---
+++ frontend/src/app/mock-articles.ts
@@ -0,0 +1,7 @@
+import { Article } from './article';
+
+export const Articles: Article [] = [
+ { id: 1, title: 'article', description: 'nice article', URL: 'www.article.com' },
+ { id: 2, title: 'article2', description: 'nicer article', URL: 'www.article.com' },
+ { id: 3, title: 'article3', description: 'nicest article', URL: 'www.article.com' }
+];
