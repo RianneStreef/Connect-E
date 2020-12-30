a900f6b8905bdafa2bb3b2b65642ba3f28610549
Martin Kruger <martin@martinkruger.com>
2020-12-30 15:45:58 +0200

    Backend

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+// const mongoose = require('mongoose');
 const app = express();
 
 const mysql = require('mysql');
 
-const path = require("path");
+const path = require('path');
 
-const articleRoutes = require("./routes/article");
-const userRoutes = require("./routes/user");
+const articleRoutes = require('./routes/article');
+const userRoutes = require('./routes/user');
 
-// require('dotenv').config();
+require('dotenv').config();
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
 
 const con = mysql.createConnection({
-  host: "remotemysql.com",
-  user: "OXgD76ZhvJ",
-  password: "H064bkAtvN",
-  database: "OXgD76ZhvJ",
+  host: 'remotemysql.com',
+  user: 'OXgD76ZhvJ',
+  password: 'H064bkAtvN',
+  database: 'OXgD76ZhvJ',
 });
 
+console.log(process.env.TEST_VAR);
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log('Connected!');
 });
 
 // con.query("insert into articles (title, description, URL) values ('MyTitle','The title of my first article', 'www.myArticle.com')", function (err, result) {
@@ -31,25 +32,22 @@ con.connect(function(err) {
 //   console.log("Info inserted");
 // });
 
-
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   );
   next();
 });
 
-
-
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // app.use("/api/articles", articleRoutes);
 // app.use("/api/auth", userRoutes);
@@ -57,5 +55,18 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 // app.get('/api/articles', (req, res, next) => {
 //   articles
 // })
+
+app.use('/api/articles', (req, res, next) => {
+  con.query('SELECT * FROM articles', function (err, result) {
+    if (err) {
+      return res.status(400).json({
+        message: 'Unable to fetch articles',
+      });
+    }
+    return res.status(200).json({
+      articles: result,
+    });
+  });
+});
 
 module.exports = app;
